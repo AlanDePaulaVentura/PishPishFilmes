87134ce6aa6e3f6cc912bae4a2692c2f3b6633d0
AlanDePaulaVentura <159501593+AlanDePaulaVentura@users.noreply.github.com>
2024-02-13 13:37:56 -0300

    Add files via upload

--- js/filmes.js
+++ js/filmes.js
@@ -23,11 +23,11 @@ function pesquisar(event) {
     document.getElementById('resultados').style.display = 'block';
 }
 var filmes = [
-    { titulo: 'O Mundo Depois de Nós', imagem: 'img/O Mundo Depois de Nós.jpg' },
-    { titulo: 'Interestelar', imagem: 'img/Interestelar.jpg' },
+    { titulo: 'O Mundo Depois de Nós',link, imagem: 'img/O Mundo Depois de Nós.jpg' },
+    { titulo: 'Interestelar',link: 'https://embedder.net/e/tt0816692', imagem: 'img/Interestelar.jpg' },
     { titulo: 'Guardiões da Galáxia Vol. 3', imagem: 'img/Guardiões da Galáxia Vol. 3.jpg' },
-    { titulo: 'Túmulo dos Vagalumes', imagem: 'img/Tumulo dos Vagalumes.jpg' },
-    { titulo: 'Até o Último Homem', imagem: 'img/Até o Último Homem.jpg' },
+    { titulo: 'Túmulo dos Vagalumes',link:'https://archive.org/download/tumulo-dos-vagalumes-1988/T%C3%BAmulo%20dos%20Vagalumes%20%281988%29.ia.mp4', imagem: 'img/Tumulo dos Vagalumes.jpg' },
+    { titulo: 'Até o Último Homem',link: 'https://embedder.net/e/tt2119532', imagem: 'img/Até o Último Homem.jpg' },
     { titulo: 'Homem-Aranha no Aranhaverso', imagem: 'img/Homem aranha no Aranha Verso.jpg' },
     { titulo: 'Rocky Um Lutador', imagem: 'img/Rocky Um Lutador.jpg' },
     { titulo: 'Rocky 2 - A Revanche', imagem: 'img/Rocky 2 - A Revanche.jpg' },
@@ -35,13 +35,13 @@ var filmes = [
     { titulo: 'Rocky 4', imagem: 'img/Rocky 4.jpg' },
     { titulo: 'Rocky 5', imagem: 'img/Rocky 5.jpg' },
     { titulo: 'Rocky Balboa', imagem: 'img/Rocky Balboa.jpg' },
-    { titulo: 'Duna', imagem: 'img/Duna.jpg' },
-    { titulo: 'Camaleões', imagem: 'img/Camaleões.jpg' },
+    { titulo: 'Duna',link: 'https://embedder.net/e/tt1160419', imagem: 'img/Duna.jpg' },
+    { titulo: 'Camaleões',link: 'https://embedder.net/e/tt13274016', imagem: 'img/Camaleões.jpg' },
     { titulo: 'Wonka', imagem: 'img/Wonka.jpg' },
-    { titulo: 'Moonfall', imagem: 'img/Moonfall.jpg' },
-    { titulo: 'A Guerra do Amanhã', imagem: 'img/A Guerra do Amanhã.jpg' },
-    { titulo: 'A Sociedade da Neve', imagem: 'img/A Sociedade da neve.jpg' },
+    { titulo: 'Moonfall',link: 'https://embedder.net/e/tt5834426', imagem: 'img/Moonfall.jpg' },
+    { titulo: 'A Guerra do Amanhã',link: 'https://embedder.net/e/tt9777666', imagem: 'img/A Guerra do Amanhã.jpg' },
+    { titulo: 'A Sociedade da Neve',link: 'https://embedder.net/e/tt16277242', imagem: 'img/A Sociedade da neve.jpg' },
     { titulo: 'O Vendedor de Passados', imagem: 'img/O Vendedor de Passados.jpg' },
-    { titulo: 'Wall-E', imagem: 'img/Wall-E.jpg' }
+    { titulo: 'Wall-E',link: 'https://embedder.net/e/movie?imdb=tt0910970', imagem: 'img/Wall-E.jpg' }
     // Adicione mais filmes conforme necessário
 ];
